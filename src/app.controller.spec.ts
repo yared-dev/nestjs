221447476d3d8e312ff5027f49a9981586fe1ed8
yared-dev <yaredper@gmail.com>
2021-07-22 21:36:32 -0500

    1.1

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 describe('AppController', () => {
   let appController: AppController;
 
@@ -10,7 +9,6 @@ describe('AppController', () => {
       controllers: [AppController],
       providers: [AppService],
     }).compile();
-
     appController = app.get<AppController>(AppController);
   });
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOption = {
+    origin: ['http://localhost:8080'],
+  };
+  app.enableCors(corsOption);
+  app.use(morgan('dev'));
   await app.listen(3000);
 }
 bootstrap();
